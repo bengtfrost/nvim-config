751cf4b2f719bb57f0a52a28b3ac82bdefd0c559
bengtfrost <bengtfrost@gmail.com>
2025-04-20 18:57:05 +0200

    modified:   lua/plugins/colorscheme.lua

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,40 +1,51 @@
 -- lua/plugins/colorscheme.lua
+-- Replace your existing colorscheme file content with this
+
 return {
-  {
-    'EdenEast/nightfox.nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require('nightfox').setup({
-        options = {
-          transparent = false,
-          terminal_colors = true,
-          dim_inactive = false,
-          module_default = true,
+  "navarasu/onedark.nvim",
+  priority = 1000, -- Ensure it loads early
+  config = function()
+    require("onedark").setup({
+      -- Default theme style. Choose between 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer'
+      style = "dark", -- Or 'darker', 'cool', etc.
+      transparent = false, -- Show/hide background
+      term_colors = true, -- Change terminal color scheme
+      ending_tildes = false, -- Show the end-of-buffer tildes
+      cmp_itemkind_reverse = false, -- Reverse item kind highlights in cmp menu
 
-          -- Customize general appearance
-          styles = {
-            comments = "italic",
-            constants = "bold",
-            keywords = "italic",
-            -- functions = "none", -- REMOVE THIS LINE
-            -- variables = "none", -- REMOVE THIS LINE
-            types = "italic",
-          },
+      -- Code styles
+      code_style = {
+        comments = "italic",
+        keywords = "none",
+        functions = "none",
+        strings = "none",
+        variables = "none",
+      },
 
-          inverse = {
-            visual = false,
-            search = false,
-          },
-          modules = {}
-        },
-        palettes = {},
-        groups = {}
-      })
+      -- Lualine options (if you use lualine)
+      lualine = {
+        transparent = false, -- lualine center bar transparency
+      },
 
-      -- Load the carbonfox variant
-      vim.cmd.colorscheme 'carbonfox'
+      -- Custom highlights (example)
+      -- highlights = {
+      --   ["@comment"] = { italic = true },
+      --   ["@keyword"] = { bold = true },
+      -- },
 
-    end,
-  },
+      -- Plugins setup
+      -- You can disable integrations if needed
+      diagnostics = {
+         darker = true, -- darker colors for diagnostic warnings etc.
+         undercurl = true,
+         background = true,
+      },
+    })
+
+    -- Load the colorscheme
+    vim.cmd.colorscheme("onedark")
+
+    -- Optional: You can setup highlight groups here if needed
+    -- Example: vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+  end,
 }
